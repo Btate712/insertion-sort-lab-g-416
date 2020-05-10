84f07db9f4d6c80b48ca879f5ab9b09983a96670
Bob Tate <btate712@gmail.com>
2020-05-09 19:04:16 -0500

    solved

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
-
+  let minIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[minIndex]) {
+      minIndex = i;
+    }
+  }
+  const min = array.splice(minIndex, 1)[0];
+  console.log(min, array);
+  return min;
 }
 
 function insertionSort(array){
-
+  const returnArray = [];
+  let numPasses = array.length;
+  for(let i = 0; i < numPasses; i++) {
+    console.log("Pass", i);
+    returnArray.push(findMinAndRemove(array));
+  }
+  return returnArray;
 }
